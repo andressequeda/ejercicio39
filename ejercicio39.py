18ff2eddd91f64d4aab06334887559b7b261b283
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-10 10:00:49 -0500

    Add files via upload

---
+++ ejercicio39.py
@@ -0,0 +1,17 @@
+dia  =  int ( input ( "Ingrese el número de algún día de la semana (1 a 7):" ))
+si  dia  ==  1 :
+    print ( "El día es lunes." )
+elif  dia  ==  2 :
+    print ( "El día es martes." )
+elif  dia  ==  3 :
+    print ( "El día es miércoles." )
+elif  dia  ==  4 :
+    print ( "El día es jueves." )
+elif  dia  ==  5 :
+    print ( "El día es viernes." )
+elif  dia  ==  6 :
+    print ( "El día es sábado." )
+elif  dia  ==  7 :
+    print ( "El día es domingo." )
+otra cosa :
+    print ( "ERROR (ingresó un número que no está entre 1 y 7)." )
